aa614c488989ee46a187b5b39f777acb5f3eca03
ilya <intracer@gmail.com>
2015-03-09 20:32:04 +0200

    remove testing code

--- src/main/scala/org/scalawiki/MwBot.scala
+++ src/main/scala/org/scalawiki/MwBot.scala
@@ -11,7 +11,6 @@ import spray.can.Http
 import spray.http._
 import spray.util._
 
-import scala.collection.SortedSet
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
@@ -150,7 +149,7 @@ object MwBot {
   import spray.caching.{Cache, LruCache}
 
   import scala.concurrent.ExecutionContext.Implicits.global
-  import scala.concurrent.{Future, _}
+  import scala.concurrent._
 
   val commons = "commons.wikimedia.org"
   val ukWiki = "uk.wikipedia.org"
@@ -168,164 +167,8 @@ object MwBot {
 
   def get(host: String): MwBot = {
     Await.result(cache(host) {
-      future { create(host) }
+      Future { create(host) }
     }, 1.minute)
-  }
-
-  def main(args: Array[String]) {
-
-
-//        val ukWiki = new MwBot(http, system, "uk.wikipedia.org")
-
-  //   val result =   Await.result(ukWiki.login(args(0), args(1)), http.timeout)
-    //    listsNew(system, http, ukWiki)
-    //Monument.lists(ukWiki, "ВЛЗ-рядок")
-
-    val commons = create("commons.wikimedia.org")
-
-//    commons.await(commons.login(args(0), args(1)))
-    //    images(commons)
-//    imagesText(commons)
-    //imagesInfo(commons)
-
-   // contestImages(commons)
-
-//    imagesIds(commons)
-    imagesAuthors(commons)
-
-  }
-
-  def contestImages(commons: MwBot)(implicit dispatcher: ExecutionContext) {
-    commons.page("Category:Images from Wiki Loves Earth 2014").categoryMembers(Set(Namespace.CATEGORY)) flatMap {
-      categories =>
-        val filtered = categories.filter(c => c.title.startsWith("Category:Images from Wiki Loves Earth 2014 in"))
-        Future.traverse(filtered)(
-          category => commons.page(category.id).categoryMembers(Set(Namespace.FILE))
-        )
-    } map {
-      filesInCategories =>
-
-        for (files1 <- filesInCategories;
-             files2 <- filesInCategories) {
-          if (files1 != files2) {
-            val intersect = files1.intersect(files2)
-            if (intersect.nonEmpty) {
-              println(intersect)
-            }
-          }
-        }
-
-        commons.shutdown()
-
-    }
-  }
-
-  def images(commons: MwBot)(implicit dispatcher: ExecutionContext) {
-    commons.page("Category:Images from Wiki Loves Earth 2014 in Ukraine").categoryMembers() map {
-      pages =>
-        println("pages:" + pages.size)
-
-        commons.shutdown()
-    }
-  }
-
-  def imagesIds(commons: MwBot)(implicit dispatcher: ExecutionContext) {
-    commons.page("Category:Images from Wiki Loves Earth 2014 in Ukraine").revisionsByGenerator("categorymembers", "cm",
-      Set.empty, Set("content", "timestamp", "user", "comment")) map {
-      pages =>
-
-        val idRegex = """(\d\d)-(\d\d\d)-(\d\d\d\d)"""
-       val ids:Seq[String] = pages.flatMap(_.text.map(Template.getDefaultParam(_, "UkrainianNaturalHeritageSite")))
-        .filter(_.matches(idRegex))
-
-        val byId = ids.groupBy(identity)
-        println("pages:" + pages.size)
-        println("ids:" + byId.size)
-
-        commons.shutdown()
-    }
-  }
-
-  def imagesAuthors(commons: MwBot)(implicit dispatcher: ExecutionContext) {
-    val wle2014 = getAuthors(commons, "Category:Images from Wiki Loves Earth 2014 in Ukraine")
-    val wle2013 = getAuthors(commons, "Category:Images from Wiki Loves Earth 2013 in Ukraine")
-
-    val wlm2013 = getAuthors(commons, "Category:Images from Wiki Loves Monuments 2013 in Ukraine")
-    val wlm2012 = getAuthors(commons, "Category:Images from Wiki Loves Monuments 2012 in Ukraine")
-
-    val all = wlm2012 ++ wlm2013 ++ wle2013 ++ wle2014
-
-    println(all.size)
-
-    val mapped = all.map(u => s"{{#target:User talk:$u}}").mkString("\n")
-
-    println(mapped)
-
-  }
-
-  def getAuthors(commons: MwBot, category: String)(implicit dispatcher: ExecutionContext): SortedSet[String] = {
-    val result = Await.result(commons.page(category).revisionsByGenerator("categorymembers", "cm",
-      Set.empty, Set("content", "timestamp", "user", "comment")) map {
-      pages => getAuthors(pages)
-    }, 15.minutes)
-
-    Thread.sleep(1000)
-    result
-  }
-
-  def getAuthors(pages: Seq[Page])(implicit dispatcher: ExecutionContext): SortedSet[String] = {
-    val authors: Seq[String] = pages.flatMap(_.text.map(t => new Template(t).getParam("author")))
-
-    val byAuthor = authors.groupBy(identity)
-    println("pages:" + pages.size)
-    println("authors:" + byAuthor.size)
-    val authorsSet = SortedSet(byAuthor.keys.map(t => t.split("\\|")(0).replace("[[User:", "")).toSeq: _*)
-    println("authors:" + authorsSet.size)
-//    println(authorsSet.mkString("\n"))
-
-    authorsSet
-  }
-
-  def imagesInfo(commons: MwBot)(implicit dispatcher: ExecutionContext) {
-    commons.page("Category:Images from Wiki Loves Earth 2014 in Ukraine").imageInfoByGenerator("categorymembers", "cm") map {
-      pages =>
-        println("pages:" + pages.size)
-
-        commons.shutdown()
-    }
-  }
-
-  def listsOld(ukWiki: MwBot)(implicit dispatcher: ExecutionContext) {
-    ukWiki.page("Template:ВЛЗ-рядок").whatTranscludesHere() flatMap {
-      pages =>
-        println("pages:" + pages.size)
-        Future.traverse(pages)(page => ukWiki.pageText(page.title).map(page.withText))
-    } map {
-      pages =>
-        pages.foreach { page =>
-          val text: String = page.text.getOrElse("")
-          val monuments = "\\{\\{ВЛЗ-рядок".r.findAllMatchIn(text).size
-          println(s"${page.title} - $monuments monuments")
-        }
-
-        ukWiki.shutdown()
-    }
-  }
-
-  def listsNew(ukWiki: MwBot)(implicit dispatcher: ExecutionContext) {
-    ukWiki.page("Template:ВЛП-рядок").revisionsByGenerator("embeddedin", "ei", Set.empty, Set("content", "timestamp", "user", "comment")) map {
-      pages =>
-        println("pages2:" + pages.size)
-        for (page <- pages) {
-          val text: String = page.text.getOrElse("")
-          val monuments = "\\{\\{ВЛП-рядок".r.findAllMatchIn(text).size
-          println(s"${page.title} - $monuments monuments")
-        }
-
-        //          val commons = new MwBot(http, system, "commons.wikimedia.org")
-        ukWiki.shutdown()
-    }
-    // commons.shutdown()
   }
 
 
