4dc9c7bf470639e98c589eebfeae58e3ab5ad502
ilya <intracer@gmail.com>
2015-04-30 17:52:07 +0300

    table spec

--- src/main/scala/org/scalawiki/dto/markup/Table.scala
+++ src/main/scala/org/scalawiki/dto/markup/Table.scala
@@ -9,43 +9,51 @@ class Table(
 
 
   def asWiki = {
-    s"{| class='$cssClass'\n" +
-      s"|+ $title\n" +
-      headers.mkString("! ", " !! ", "") +
-      data.map(row => row.mkString("| ", " || ", "")).mkString("\n|-\n", "\n|-\n", "\n|}")
+    "{|" +
+      (if (cssClass.isEmpty) "" else s" class='$cssClass'") +
+      (if (title.isEmpty) "" else s"\n|+ $title") +
+      (if (headers.isEmpty) "" else headers.mkString("\n! ", " !! ", "")) +
+      (if (data.isEmpty) ""
+      else {
+        data.map {
+          row =>
+            row.mkString("| ", " || ", "")
+        }.mkString("\n|-\n", "\n|-\n", "")
+      }) +
+      "\n|}"
   }
 
 
-//  def asHtml = {
-//    s"{| class=\"$cssClass\"\n" +
-//      s"|+ $title\n" +
-//      headers.mkString("! ", " !! ", "") +
-//      data.map(row => row.mkString("| ", " || ", "")).mkString("\n|-\n", "\n|-\n", "\n|}")
-//
-//
-//    val x = s"<table class=\"$cssClass\">"
-//      <thead>
-//        <tr>
-//          {}
-//          <th>@Messages("id")</th>
-//          <th>@Messages("image")</th>
-//          <th>@Messages("name")</th>
-//          <th>@Messages("year")</th>
-//          <th>@Messages("place")</th>
-//          <th>@Messages("city")</th>
-//          <th>@Messages("user")</th>
-//          <th>@Messages("coordinates")</th>
-//          <th>@Messages("page")</th>
-//          <th>@Messages("type")</th>
-//          <th>@Messages("commons")</th>
-//          <th>@Messages("resolution")</th>
-//        </tr>
-//      </thead>
-//      <tbody>
-//      </tbody>
-//    </table>
+  //  def asHtml = {
+  //    s"{| class=\"$cssClass\"\n" +
+  //      s"|+ $title\n" +
+  //      headers.mkString("! ", " !! ", "") +
+  //      data.map(row => row.mkString("| ", " || ", "")).mkString("\n|-\n", "\n|-\n", "\n|}")
+  //
+  //
+  //    val x = s"<table class=\"$cssClass\">"
+  //      <thead>
+  //        <tr>
+  //          {}
+  //          <th>@Messages("id")</th>
+  //          <th>@Messages("image")</th>
+  //          <th>@Messages("name")</th>
+  //          <th>@Messages("year")</th>
+  //          <th>@Messages("place")</th>
+  //          <th>@Messages("city")</th>
+  //          <th>@Messages("user")</th>
+  //          <th>@Messages("coordinates")</th>
+  //          <th>@Messages("page")</th>
+  //          <th>@Messages("type")</th>
+  //          <th>@Messages("commons")</th>
+  //          <th>@Messages("resolution")</th>
+  //        </tr>
+  //      </thead>
+  //      <tbody>
+  //      </tbody>
+  //    </table>
 
- // }
+  // }
 
 
 }
--- src/test/scala/org/scalawiki/dto/markup/TableSpec.scala
+++ src/test/scala/org/scalawiki/dto/markup/TableSpec.scala
@@ -4,4 +4,82 @@ import org.specs2.mutable.Specification
 
 class TableSpec extends Specification {
 
+  "asWiki" should {
+    "make empty table" in {
+      val table = new Table("", Seq.empty, Seq.empty, "")
+      table.asWiki === "{|\n|}"
+    }
+
+    "make table with css class" in {
+      val table = new Table("", Seq.empty, Seq.empty, "wikitable")
+      table.asWiki === "{| class='wikitable'\n|}"
+    }
+
+    "make table with title" in {
+      val table = new Table("title", Seq.empty, Seq.empty, "")
+      table.asWiki === "{|\n|+ title\n|}"
+    }
+
+    "make table with 1 column header" in {
+      val table = new Table("", Seq("header1"), Seq.empty, "")
+      table.asWiki === "{|\n! header1\n|}"
+    }
+
+    "make table with 2 columns header" in {
+      val table = new Table("", Seq("header1", "header2"), Seq.empty, "")
+      table.asWiki === "{|\n! header1 !! header2\n|}"
+    }
+
+    "make table with 3 columns headers" in {
+      val table = new Table("", Seq("header1", "header2", "header3"), Seq.empty, "")
+      table.asWiki === "{|\n! header1 !! header2 !! header3\n|}"
+    }
+
+    "make table with 1 data column" in {
+      val table = new Table("", Seq.empty, Seq(Seq("data11")), "")
+      table.asWiki === "{|\n|-\n| data11\n|}"
+    }
+
+    "make table with 2 data columns" in {
+      val table = new Table("", Seq.empty, Seq(Seq("data11", "data12")), "")
+      table.asWiki === "{|\n|-\n| data11 || data12\n|}"
+    }
+
+    "make table with 1 data column and 2 rows" in {
+      val table = new Table("", Seq.empty, Seq(Seq("data11"), Seq("data21")), "")
+      table.asWiki === "{|\n|-\n| data11\n|-\n| data21\n|}"
+    }
+
+    "make table with css class and title" in {
+      val table = new Table("title", Seq.empty, Seq.empty, "wikitable")
+      table.asWiki === "{| class='wikitable'\n|+ title\n|}"
+    }
+
+    "make table with css class and 1 column header" in {
+      val table = new Table("", Seq("header1"), Seq.empty, "wikitable")
+      table.asWiki === "{| class='wikitable'\n! header1\n|}"
+    }
+
+    "make table with css class and 1 data column" in {
+      val table = new Table("", Seq.empty, Seq(Seq("data11")), "wikitable")
+      table.asWiki === "{| class='wikitable'\n|-\n| data11\n|}"
+    }
+
+    "make table with title and 1 column header" in {
+      val table = new Table("title", Seq("header1"), Seq.empty, "")
+      table.asWiki === "{|\n|+ title\n! header1\n|}"
+    }
+
+    "make table with title and 1 data column" in {
+      val table = new Table("title", Seq.empty, Seq(Seq("data11")), "")
+      table.asWiki === "{|\n|+ title\n|-\n| data11\n|}"
+    }
+
+    "make table with 1 column header and 1 data row" in {
+      val table = new Table("", Seq("header1"), Seq(Seq("data11")), "")
+      table.asWiki === "{|\n! header1\n|-\n| data11\n|}"
+    }
+
+  }
+
 }
