b3a8bc9d765878afa95f509b6274c4c2ed8800f1
ilya <intracer@gmail.com>
2016-03-23 09:19:41 +0200

    parametrize JavaFxBrowser in descendants

--- scalawiki-bots/src/main/scala/org/scalawiki/desktop/JavaFxBrowser.scala
+++ scalawiki-bots/src/main/scala/org/scalawiki/desktop/JavaFxBrowser.scala
@@ -7,19 +7,31 @@ import scalafx.scene.control.ScrollPane
 import scalafx.scene.layout.VBox
 import scalafx.scene.web.WebView
 
-object JavaFxBrowser extends JFXApp {
-  stage = new PrimaryStage {
-    title = "ScalaWiki"
-    width = 500
-    height = 500
-    scene = new Scene {
-      root = new VBox {
-        children = Seq(new ScrollPane {
-          content = new WebView {
-            engine.loadContent("<b>Hello ScalaWiki</b>")
-          }
-        })
+trait JavaFxBrowser extends JFXApp {
+
+  def load(browser: WebView)
+
+  def openStage() = {
+    val browser = new WebView()
+    stage = new PrimaryStage {
+      title = "ScalaWiki"
+      scene = new Scene {
+        root = new VBox {
+          children = Seq(new ScrollPane {
+            content = browser
+            load(browser)
+          })
+        }
       }
     }
+  }
+}
+
+object JavaFxBrowserMain extends JavaFxBrowser {
+
+  openStage()
+
+  override def load(browser: WebView) = {
+    browser.engine.loadContent("<b>Hello ScalaWiki</b>")
   }
 }
