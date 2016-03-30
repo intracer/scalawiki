9ad8dc37150f1ec1c7128b0b0829381e80d41811
ilya <intracer@gmail.com>
2016-03-30 17:42:54 +0300

    PageFromFileBotSpec - process reasonable amount of pages in reasonable amount of memory

--- scalawiki-bots/src/test/scala/org/scalawiki/bots/PageFromFileBotSpec.scala
+++ scalawiki-bots/src/test/scala/org/scalawiki/bots/PageFromFileBotSpec.scala
@@ -4,7 +4,7 @@ import org.specs2.mutable.Specification
 
 class PageFromFileBotSpec extends Specification {
 
-  val docExampleDefaultParams =
+  val docExample =
     """{{-start-}}
       |'''PageName'''
       |Text here
@@ -15,7 +15,7 @@ class PageFromFileBotSpec extends Specification {
       |Another text
       |{{-end-}}""".stripMargin
 
-  val docExample =
+  val docExampleOwnDelimiter =
     """xxxx
       |'''PageName'''
       |Text here
@@ -29,7 +29,7 @@ class PageFromFileBotSpec extends Specification {
   "page from file" should {
 
     "support default start and end parameters" in {
-      val pages = PageFromFileBot.pages(docExampleDefaultParams).toBuffer
+      val pages = PageFromFileBot.pages(docExample).toBuffer
       pages.size === 2
       pages.map(_.title) === Seq("PageName", "AnotherPageName")
       pages.flatMap(_.text) === Seq(
@@ -39,13 +39,40 @@ class PageFromFileBotSpec extends Specification {
     }
 
     "support start and end parameters" in {
-      val pages = PageFromFileBot.pages(docExample, start = "xxxx", end = "yyyy").toBuffer
+      val pages = PageFromFileBot.pages(docExampleOwnDelimiter, start = "xxxx", end = "yyyy").toBuffer
       pages.size === 2
       pages.map(_.title) === Seq("PageName", "AnotherPageName")
       pages.flatMap(_.text) === Seq(
         "'''PageName'''\nText here\n",
         "'''AnotherPageName'''\nAnother text"
       )
+    }
+
+    "process many pages in" in {
+      def mb(bytes: Long): Double = bytes / Math.pow(2, 20)
+      val runtime = Runtime.getRuntime
+
+      val maxMemMB = mb(runtime.maxMemory)
+      val words = 50
+      val lines = 10
+      val articles = Math.min(5000, 5000 * (maxMemMB / 200)).toInt
+      println(s"$maxMemMB MB heap should be enough to process at least $articles articles")
+
+      new StringBuilder().append()
+      val text = Array.fill(lines) {
+        Array.fill(words)("wikitext").mkString(" ")
+      }.mkString("{{-start-}}\n'''PageName''' ", "\n", "\n{{-end-}}")
+
+      val chunk = Array.fill(articles)(text).mkString("\n")
+
+      val pages = PageFromFileBot.pages(chunk)
+      val size = pages.size
+
+      size === articles
+
+      val used  = mb(runtime.totalMemory() - runtime.freeMemory()).toInt
+      println(s"Used $used MB")
+      used should be lessThan 200
     }
   }
 }
