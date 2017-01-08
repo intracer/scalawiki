5e5a8d4b32e741225f7fd536032543c2d911f6e4
intracer <intracer@gmail.com>
2017-01-08 09:28:05 +0200

    wlx: updated test

--- scalawiki-wlx/src/test/scala/org/scalawiki/wlx/stat/StatisticsSpec.scala
+++ scalawiki-wlx/src/test/scala/org/scalawiki/wlx/stat/StatisticsSpec.scala
@@ -75,7 +75,7 @@ class StatisticsSpec(implicit ee: ExecutionEnv) extends Specification with Mocki
     val monuments = Seq(new Monument(id = "123", name = "123 monument"))
 
     val stat = mockedStat(monuments, images)
-    val data = stat.gatherData(total = false, byYear = false).await
+    val data = stat.gatherData(total = false).await
 
     data.contest === contest
     data.monumentDb.map(_.monuments) === Some(monuments)
