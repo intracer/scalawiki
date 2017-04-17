cbe360794ca3e8c02a666fe0ebd26cd3a302361f
ilya <intracer@gmail.com>
2017-04-17 19:43:47 +0300

    some scaladocs and formatting

--- scalawiki-wlx/src/main/scala/org/scalawiki/wlx/ShortLinksBot.scala
+++ scalawiki-wlx/src/main/scala/org/scalawiki/wlx/ShortLinksBot.scala
@@ -13,6 +13,9 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.io.Source
 
+/**
+  * Converts links to Wikimedia Commons files to short links (with file id) and tries to add image author and license
+  */
 object ShortLinksBot {
   val bot = MwBot.fromHost(MwBot.commons)
 
@@ -28,29 +31,29 @@ object ShortLinksBot {
   }
 
   def getPageLicense(page: Page): Option[String] = {
-      for (id <- page.id;
-           text <- page.revisions.headOption.flatMap(_.content)) yield {
+    for (id <- page.id;
+         text <- page.revisions.headOption.flatMap(_.content)) yield {
 
-        val author = Image.getAuthorFromPage(text)
+      val author = Image.getAuthorFromPage(text)
 
-        val license = text
-          .split("\\s|\\||\\{|\\}")
-          .map(_.toLowerCase)
-          .find { s =>
-            s.startsWith("cc-") ||
-              s.startsWith("gfdl") ||
-              s.startsWith("wikimapia")
-          }
-          .getOrElse("???")
+      val license = text
+        .split("\\s|\\||\\{|\\}")
+        .map(_.toLowerCase)
+        .find { s =>
+          s.startsWith("cc-") ||
+            s.startsWith("gfdl") ||
+            s.startsWith("wikimapia")
+        }
+        .getOrElse("???")
 
-        val readableLicense = license
-          .replace("cc-by-sa-", "CC BY-SA ")
-          .replace("cc-zero", "CC0 1.0")
-          .replace("gfdl-self", "GFDL")
-          .replace("wikimapia", "CC BY-SA 3.0")
+      val readableLicense = license
+        .replace("cc-by-sa-", "CC BY-SA ")
+        .replace("cc-zero", "CC0 1.0")
+        .replace("gfdl-self", "GFDL")
+        .replace("wikimapia", "CC BY-SA 3.0")
 
-        s"https://commons.wikimedia.org/?curid=$id © $author, $readableLicense"
-      }
+      s"https://commons.wikimedia.org/?curid=$id © $author, $readableLicense"
+    }
   }
 
   def getLineInfo(line: String): Future[String] = {
@@ -63,13 +66,13 @@ object ShortLinksBot {
   }
 
   def getFileSubstring(line: String): Future[String] = {
-      val replaced = line.replace("%D0%A4%D0%B0%D0%B9%D0%BB:", "File:")
-      val start = replaced.indexOf("File:")
-      if (start >= 0) {
-        val decoded = URLDecoder.decode(replaced.substring(start), "UTF-8")
-        getPage(decoded.trim).map(_.flatMap(getPageLicense).getOrElse(line))
-      }
-      else Future.successful(line)
+    val replaced = line.replace("%D0%A4%D0%B0%D0%B9%D0%BB:", "File:")
+    val start = replaced.indexOf("File:")
+    if (start >= 0) {
+      val decoded = URLDecoder.decode(replaced.substring(start), "UTF-8")
+      getPage(decoded.trim).map(_.flatMap(getPageLicense).getOrElse(line))
+    }
+    else Future.successful(line)
   }
 
   def main(args: Array[String]) {
