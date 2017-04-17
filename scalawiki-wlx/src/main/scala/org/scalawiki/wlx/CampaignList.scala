24ea772616772843ecb52be36fcec7b71cf044cc
ilya <intracer@gmail.com>
2017-04-17 20:56:01 +0300

    CampaignList WIP - get  each year's category members

--- scalawiki-wlx/src/main/scala/org/scalawiki/wlx/CampaignList.scala
+++ scalawiki-wlx/src/main/scala/org/scalawiki/wlx/CampaignList.scala
@@ -1,9 +1,9 @@
 package org.scalawiki.wlx
 
-import org.scalawiki.dto.Namespace
+import org.scalawiki.dto.{Namespace, Page}
 import org.scalawiki.dto.cmd.Action
 import org.scalawiki.dto.cmd.query.Query
-import org.scalawiki.dto.cmd.query.list.{CategoryMembers, CmNamespace, CmTitle, ListParam}
+import org.scalawiki.dto.cmd.query.list._
 import org.scalawiki.wlx.dto.ContestType
 import org.scalawiki.{MwBot, WithBot}
 
@@ -16,18 +16,26 @@ object CampaignList extends WithBot {
 
   def categoryMembers(parent: String): Action = {
     Action(Query(ListParam(CategoryMembers(
-      CmTitle(parent), CmNamespace(Seq(Namespace.CATEGORY))
-    ))))
+      CmTitle(parent), CmNamespace(Seq(Namespace.CATEGORY)), CmLimit("max"))
+    )))
   }
+
+  def categoriesMembers(categories: Seq[String]): Future[Seq[Seq[Page]]] = {
+    val queries = categories.map(categoryMembers)
+    Future.sequence(queries.map(bot.run(_)))
+  }
+
+  def titles(pages: Seq[Seq[Page]]) = pages.flatten.map(_.title)
 
   def main(args: Array[String]): Unit = {
     val types = ContestType.all
     val contestCats = types.map(_.imagesCategory)
-    val queries = contestCats.map(categoryMembers)
 
-    for (yearCats <- Future.sequence(queries.map(bot.run(_)))) {
-      val imageCats = yearCats.flatten.map(_.title).filter(CountryParser.isContestCategory)
-      imageCats.sorted.foreach(println)
+    for (yearCats <- categoriesMembers(contestCats)) {
+      val imageCats = titles(yearCats).filter(CountryParser.isContestCategory)
+      for (campaignCats <- categoriesMembers(imageCats)) {
+        titles(campaignCats).foreach(println)
+      }
     }
   }
 
