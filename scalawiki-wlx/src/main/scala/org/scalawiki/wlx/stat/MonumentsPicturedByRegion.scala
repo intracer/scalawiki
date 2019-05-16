26d60f6d7ef1c255132ddf85468878c67fed923a
IKorniiko <IKorniiko@luxoft.com>
2019-05-16 18:27:24 +0300

    wlx: simplify monumentsPicturedTable

--- scalawiki-wlx/src/main/scala/org/scalawiki/wlx/stat/MonumentsPicturedByRegion.scala
+++ scalawiki-wlx/src/main/scala/org/scalawiki/wlx/stat/MonumentsPicturedByRegion.scala
@@ -2,7 +2,6 @@ package org.scalawiki.wlx.stat
 import org.jfree.data.category.DefaultCategoryDataset
 import org.scalawiki.MwBot
 import org.scalawiki.dto.markup.Table
-import org.scalawiki.wlx.dto.Monument
 import org.scalawiki.wlx.{ImageDB, MonumentDB}
 
 class MonumentsPicturedByRegion(val stat: ContestStat, uploadImages: Boolean = false)  extends Reporter {
@@ -20,56 +19,41 @@ class MonumentsPicturedByRegion(val stat: ContestStat, uploadImages: Boolean = f
 
   val name = "Monuments pictured by region"
 
-  var images: Option[String] = None
-  val charts = new Charts()
+  val yearSeq = stat.yearSeq
 
   override def asText: String = {
 
     val header = s"\n==$name==\n"
 
-    header + table.asWiki + images.getOrElse("")
+    header + table.asWiki + regionalStatImages().getOrElse("")
   }
 
   def table = monumentsPicturedTable(stat.dbsByYear, stat.totalImageDb, stat.monumentDb.get)
 
   def monumentsPicturedTable(imageDbs: Seq[ImageDB], totalImageDb: Option[ImageDB], monumentDb: MonumentDB): Table = {
-    val contest = monumentDb.contest
-    val categoryName = contest.imagesCategory
-    val filenamePrefix = contest.name.replace("_", "")
 
-    val yearSeq = stat.yearSeq
-    val numYears = yearSeq.size
-
-    val yearsColumns = yearSeq.flatMap { year =>
+    val columns = {
+      val numYears = yearSeq.size
+      val yearsColumns = yearSeq.flatMap { year =>
         val ys = year.toString
         Seq(ys + " Objects", ys + " Pictures")
+      }
+
+      Seq("Region", "Objects in lists", s"$numYears years total", s"$numYears years percentage") ++ yearsColumns
     }
 
-    val columns = Seq("Region", "Objects in lists",
-      s"$numYears years total", s"$numYears years percentage") ++ yearsColumns
-
-    val dataset = new DefaultCategoryDataset()
-
-    val regionIds = monumentDb.regionIds
-
-    val withPhotoInLists = monumentDb.monuments.filter(_.photo.isDefined).map(_.id).toSet
-
-    val rows = regionIds.map { regionId =>
-
-      val withPhotoInListsCurrentRegion = withPhotoInLists.filter(id => Monument.getRegionId(id) == regionId)
+    def regionData(regionId:String) = {
       val picturedMonumentsInRegionSet = totalImageDb.map(_.idsByRegion(regionId)).getOrElse(Set.empty) ++
-        withPhotoInListsCurrentRegion
+        monumentDb.picturedInRegion(regionId)
       val picturedMonumentsInRegion = picturedMonumentsInRegionSet.size
       val allMonumentsInRegion = monumentDb.byRegion(regionId).size
-
-      val picturedIds = stat.mapYears(_.idsByRegion(regionId).size)
 
       val pictured = stat.mapYears { db =>
         Seq(
           db.idsByRegion(regionId).size,
           db.imagesByRegion(regionId).toSet.size
         )
-      }
+      }.flatten
 
       val regionName = contest.country.regionName(regionId)
       val columnData = (Seq(
@@ -78,14 +62,14 @@ class MonumentsPicturedByRegion(val stat: ContestStat, uploadImages: Boolean = f
         picturedMonumentsInRegion,
         100 * picturedMonumentsInRegion / allMonumentsInRegion) ++ pictured).map(_.toString)
 
-      val shortRegionName = regionName.replaceAll("область", "").replaceAll("Автономна Республіка", "АР")
-      picturedIds.zipWithIndex.foreach { case (n, i) => dataset.addValue(n, yearSeq(i), shortRegionName) }
-
       columnData
     }
 
+    val regionIds = monumentDb.regionIds
+    val rows = regionIds.map(regionData)
+
     val allMonuments = monumentDb.monuments.size
-    val picturedMonuments = (totalImageDb.map(_.ids).getOrElse(Set.empty) ++ withPhotoInLists).size
+    val picturedMonuments = (totalImageDb.map(_.ids).getOrElse(Set.empty) ++ monumentDb.picturedIds).size
 
     val ids = stat.mapYears(_.ids)
 
@@ -101,11 +85,28 @@ class MonumentsPicturedByRegion(val stat: ContestStat, uploadImages: Boolean = f
       picturedMonuments.toString,
       (if (allMonuments != 0) 100 * picturedMonuments / allMonuments else 0).toString) ++ totalByYear.map(_.toString)
 
-    images = Some(regionalStatImages(filenamePrefix, categoryName, yearSeq, dataset, ids, idsSize, uploadImages))
-
-    new Table(columns, rows ++ Seq(totalData), "Objects pictured")
+    Table(columns, rows ++ Seq(totalData), "Objects pictured")
   }
 
+  def regionalStatImages(): Some[String] = {
+    val dataset = new DefaultCategoryDataset()
+
+    stat.monumentDb.get.regionIds.foreach { regionId =>
+      val picturedIds = stat.mapYears(_.idsByRegion(regionId).size)
+      val regionName = contest.country.regionName(regionId)
+
+      val shortRegionName = regionName.replaceAll("область", "").replaceAll("Автономна Республіка", "АР")
+      picturedIds.zipWithIndex.foreach { case (n, i) => dataset.addValue(n, yearSeq(i), shortRegionName) }
+    }
+
+    val ids = stat.mapYears(_.ids)
+    val idsSize = ids.map(_.size)
+
+    val categoryName = contest.imagesCategory
+    val filenamePrefix = contest.name.replace("_", "")
+
+    Some(regionalStatImages(filenamePrefix, categoryName, yearSeq, dataset, ids, idsSize, uploadImages))
+  }
 
   def regionalStatImages(
                           filenamePrefix: String,
@@ -122,6 +123,7 @@ class MonumentsPicturedByRegion(val stat: ContestStat, uploadImages: Boolean = f
         "\n<br clear=\"all\">"
 
     if (uploadImages) {
+      val charts = new Charts()
 
       val chart = charts.createChart(dataset, "Регіон")
       val byRegionFile = filenamePrefix + "PicturedByYear"
