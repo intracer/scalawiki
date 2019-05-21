9092f4e37f79da560458da742b797ad7e58a3643
ilya <intracer@gmail.com>
2019-05-21 09:09:13 +0300

    less copying

--- scalawiki-wlx/src/main/scala/org/scalawiki/wlx/dto/Koatuu.scala
+++ scalawiki-wlx/src/main/scala/org/scalawiki/wlx/dto/Koatuu.scala
@@ -6,29 +6,27 @@ import play.api.libs.functional.syntax._
 
 object Koatuu {
 
-  def regionReads(level: Int): Reads[Region] = (
-    (__ \ "code").read[String] and
+  def regionReads(level: Int, parent: () => Option[AdmDivision] = () => None): Reads[Region] = (
+    (__ \ "code").read[String].map(c => shortCode(c, level)) and
       (__ \ "name").read[String].map(betterName) and
       (__ \ ("level" + level))
         .lazyReadNullable(Reads.seq[Region](regionReads(level + 1)))
         .map(_.getOrElse(Nil)).map(skipGroups) and
-      Reads.pure(() => None)
+      Reads.pure(parent)
     ) (Region)
 
   def regions(parent: () => Option[AdmDivision] = () => None): Seq[Region] = {
     val stream = getClass.getResourceAsStream("/koatuu.json")
     val json = Json.parse(stream)
 
-    implicit val level2Reads: Reads[Region] = regionReads(2)
+    implicit val level2Reads: Reads[Region] = regionReads(2, parent)
     val raw = (json \ "level1").as[Seq[Region]]
 
     raw.map { r1 =>
       r1.copy(
-        code = shortCode(r1.code),
-        regions = r1.regions
-          .map(r2 => r2.copy(code = shortCode(r2.code, 5)))
-          .map(r2 => r2.copy(parent = () => Some(r1))),
-        parent = parent
+        regions = r1.regions.map{
+          r2 => r2.copy(parent = () => Some(r1))
+        }
       )
     }
   }
@@ -47,7 +45,14 @@ object Koatuu {
     }
   }
 
-  def shortCode(s: String, init: Int = 2) = s.take(init)
+  def shortCode(s: String, level: Int) = {
+    level match {
+      case 2 => s.take(2)
+      case 3 => s.take(5)
+      case _ => s
+    }
+
+  }
 
   def betterName(s: String) = {
     val s1 = s.split("/").head.toLowerCase.capitalize
