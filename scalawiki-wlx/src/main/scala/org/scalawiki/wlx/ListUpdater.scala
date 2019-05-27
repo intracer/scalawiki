863e760a819988b59e3d278293b734de75cf32ce
ilya <intracer@gmail.com>
2019-05-27 09:46:28 +0300

    generic monument list updater

--- scalawiki-wlx/src/main/scala/org/scalawiki/wlx/ListUpdater.scala
+++ scalawiki-wlx/src/main/scala/org/scalawiki/wlx/ListUpdater.scala
@@ -1,7 +1,6 @@
 package org.scalawiki.wlx
 
 import org.scalawiki.MwBot
-import org.scalawiki.dto.Image
 import org.scalawiki.dto.markup.SwTemplate
 import org.scalawiki.edit.{PageUpdateTask, PageUpdater}
 import org.scalawiki.wikitext.SwebleParser
@@ -10,42 +9,45 @@ import org.sweble.wikitext.engine.config.WikiConfig
 import org.sweble.wikitext.engine.utils.DefaultConfigEnWp
 import org.sweble.wikitext.parser.nodes.WtTemplate
 
-object ListFiller {
+object ListUpdater {
 
-  def fillLists(monumentDb: MonumentDB, imageDb: ImageDB) {
-    val task = new ListFillerTask(MwBot.ukWiki, monumentDb, imageDb)
+  def fillLists(monumentDb: MonumentDB, monumentUpdater: MonumentUpdater) {
+    val task = new ListUpdaterTask(MwBot.ukWiki, monumentDb, monumentUpdater)
     val updater = new PageUpdater(task)
     updater.update()
   }
-
-  def bestImage(images: Seq[Image]) =
-    images.maxBy(image => image.size.get + image.width.get * image.height.get)
-
 }
 
-class ListFillerTask(val host: String, monumentDb: MonumentDB, imageDb: ImageDB) extends PageUpdateTask with SwebleParser {
+trait MonumentUpdater {
+  def needsUpdate(m: Monument): Boolean
+  def updatedParams(m: Monument): Map[String, String]
+}
+
+class ListUpdaterTask(val host: String, monumentDb: MonumentDB, monumentUpdater: MonumentUpdater) extends PageUpdateTask with SwebleParser {
 
   val config: WikiConfig = DefaultConfigEnWp.generate
 
-  val titles = pagesToFill(monumentDb, imageDb)
+  val titles = pagesToFill(monumentDb)
 
   val uploadConfig = monumentDb.contest.uploadConfigs.head
 
   override def updatePage(title: String, pageText: String): (String, String) = {
     val template = uploadConfig.listTemplate
     val wlxParser = new WlxTemplateParser(uploadConfig.listConfig, title)
-    var added: Int = 0
+    var updated: Int = 0
 
     def mapper(wtTemplate: WtTemplate) = {
       val swTemplate = new SwTemplate(wtTemplate)
       val monument = wlxParser.templateToMonument(swTemplate.template)
 
       if (needsUpdate(monument)) {
-        added += 1
-        val image = ListFiller.bestImage(imageDb.byId(monument.id))
-        val name = wlxParser.image.get
-        val value = image.title.replaceFirst("File:", "").replaceFirst("Файл:", "")
-        swTemplate.setTemplateParam(name, value)
+        updated += 1
+
+        val updates = monumentUpdater.updatedParams(monument)
+
+        updates.foreach { case (name, value) =>
+          swTemplate.setTemplateParam(name, value)
+        }
       }
     }
 
@@ -59,14 +61,13 @@ class ListFillerTask(val host: String, monumentDb: MonumentDB, imageDb: ImageDB)
       .replace("<x-comment>", "<!--")
       .replace("</x-comment>", "-->")
 
-    val comment = s"adding $added image(s)"
+    val comment = s"updated $updated monument(s)"
     (withComments, comment)
   }
 
-  def needsUpdate(m: Monument): Boolean =
-    m.photo.isEmpty && imageDb.containsId(m.id)
+  def needsUpdate(m: Monument): Boolean = monumentUpdater.needsUpdate(m)
 
-  def pagesToFill(monumentDb: MonumentDB, imageDb: ImageDB): Set[String] = {
+  def pagesToFill(monumentDb: MonumentDB): Set[String] = {
 
     val monumentsToFill = monumentDb.monuments.filter(needsUpdate)
 
@@ -77,6 +78,5 @@ class ListFillerTask(val host: String, monumentDb: MonumentDB, imageDb: ImageDB)
     println(s"pages: ${titles.size}")
     titles
   }
-
 }
 
