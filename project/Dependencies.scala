00d0ba2076ce6c36405f376f3c12e871a5573245
Scala Steward <me@scala-steward.org>
2019-09-16 16:54:10 +0200

    Update specs2-core, specs2-matcher-extra, ... to 4.7.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -125,7 +125,7 @@ object Dependencies {
     }
 
     object Specs2 {
-      val SpecsV = "4.7.0"
+      val SpecsV = "4.7.1"
 
       val core = "org.specs2" %% "specs2-core" % SpecsV
       val matcherExtra = "org.specs2" %% "specs2-matcher-extra" % SpecsV
