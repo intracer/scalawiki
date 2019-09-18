a7e04f0a5e2350676e6379726fa0c8a03ff3dd87
ilya <intracer@gmail.com>
2019-09-19 01:10:06 +0300

    fix grouping by region id spec

--- scalawiki-wlx/src/test/scala/org/scalawiki/wlx/stat/GroupingSpec.scala
+++ scalawiki-wlx/src/test/scala/org/scalawiki/wlx/stat/GroupingSpec.scala
@@ -38,18 +38,18 @@ class GroupingSpec extends Specification {
     }
 
     "group by region" in {
-      val images1 = Seq(Image("File:11.jpg", monumentIds = List("12-123")))
+      val images1 = Seq(Image("File:11.jpg", monumentIds = List("01-123")))
       val images2 = Seq(
-        Image("File:21.jpg", monumentIds = List("13-234")),
-        Image("File:22.jpg", monumentIds = List("13-234"))
+        Image("File:21.jpg", monumentIds = List("05-234")),
+        Image("File:22.jpg", monumentIds = List("05-234"))
       )
 
       val g = new Grouping("monuments", ImageGrouping.byRegion, images1 ++ images2)
 
-      g.keys === Set("12", "13")
-      g.by("12") === images1
-      g.by("13") === images2
-      g.grouped === Map("12" -> images1, "13" -> images2)
+      g.keys === Set("01", "05")
+      g.by("01") === images1
+      g.by("05") === images2
+      g.grouped === Map("01" -> images1, "05" -> images2)
     }
 
     "group by author" in {
@@ -68,26 +68,26 @@ class GroupingSpec extends Specification {
     }
 
     "group by author and region" in {
-      val imagesA1 = Seq(Image("File:A1.jpg", author = Some("A"), monumentIds = List("1-123")))
+      val imagesA1 = Seq(Image("File:A1.jpg", author = Some("A"), monumentIds = List("01-123")))
       val imagesA2 = Seq(
-        Image("File:A21.jpg", author = Some("A"), monumentIds = List("2-345")),
-        Image("File:A22.jpg", author = Some("A"), monumentIds = List("2-345"))
+        Image("File:A21.jpg", author = Some("A"), monumentIds = List("05-345")),
+        Image("File:A22.jpg", author = Some("A"), monumentIds = List("05-345"))
       )
-      val imagesB1 = Seq(Image("File:B1.jpg", author = Some("B"), monumentIds = List("1-123")))
+      val imagesB1 = Seq(Image("File:B1.jpg", author = Some("B"), monumentIds = List("01-123")))
       val imagesB2 = Seq(
-        Image("File:B21.jpg", author = Some("B"), monumentIds = List("2-345")),
-        Image("File:B22.jpg", author = Some("B"), monumentIds = List("2-345"))
+        Image("File:B21.jpg", author = Some("B"), monumentIds = List("05-345")),
+        Image("File:B22.jpg", author = Some("B"), monumentIds = List("05-345"))
       )
       val all = imagesA1 ++ imagesA2 ++ imagesB1 ++ imagesB2
 
       val byA = new Grouping("monuments", ImageGrouping.byAuthor, all)
       val byAR = byA.compose(ImageGrouping.byRegion)
 
-      byAR.by("A", "1") === imagesA1
-      byAR.by("A", "2") === imagesA2
+      byAR.by("A", "01") === imagesA1
+      byAR.by("A", "05") === imagesA2
 
-      byAR.by("B", "1") === imagesB1
-      byAR.by("B", "2") === imagesB2
+      byAR.by("B", "01") === imagesB1
+      byAR.by("B", "05") === imagesB2
     }
   }
 }
