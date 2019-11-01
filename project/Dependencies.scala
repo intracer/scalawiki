261fb9846f9f533bacbd2f33ccdb872778b9dd83
Scala Steward <me@scala-steward.org>
2019-11-01 10:30:41 +0100

    Update mockserver-netty to 5.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   val JodaTimeV = "2.9.9"
   val JSoupV = "1.12.1"
   val LogbackClassicV = "1.2.3"
-  val MockServerV = "5.3.0"
+  val MockServerV = "5.7.0"
   val ReactiveStreamsV = "1.0.2"
   val RetryV = "0.3.3"
   val Scala212V = "2.12.10"
