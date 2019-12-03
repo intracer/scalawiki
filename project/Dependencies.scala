78e0e99d55667f9b8339f76aa1af338d3d80646a
Scala Steward <me@scala-steward.org>
2019-12-03 01:06:30 +0100

    Update akka-http, akka-http-caching to 10.1.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -63,7 +63,7 @@ object Dependencies {
 
     object Akka {
       val AkkaV = "2.5.26"
-      val AkkaHttpV = "10.1.10"
+      val AkkaHttpV = "10.1.11"
 
       val actor = "com.typesafe.akka" %% "akka-actor" % AkkaV
       val stream = "com.typesafe.akka" %% "akka-stream" % AkkaV
