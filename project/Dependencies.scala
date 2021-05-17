9f50563246779c806dc26fdc3bf897267e8d2303
Scala Steward <me@scala-steward.org>
2021-05-17 12:46:34 +0200

    Update scala-library to 2.13.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val MockServerV = "5.7.0"
   val ReactiveStreamsV = "1.0.2"
   val RetryV = "0.3.3"
-  val Scala213V = "2.13.5"
+  val Scala213V = "2.13.6"
   val Scala212V = "2.12.13"
   val Scala211V = "2.11.12"
   val ScalaChartV = "0.7.1"
