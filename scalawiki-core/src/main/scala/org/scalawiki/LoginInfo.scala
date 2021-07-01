e841ed9ccd778cc7c1ee0f6780bbb719a70b36bb
ilya <intracer@gmail.com>
2021-07-01 22:19:42 +0300

    formatting

--- scalawiki-core/src/main/scala/org/scalawiki/LoginInfo.scala
+++ scalawiki-core/src/main/scala/org/scalawiki/LoginInfo.scala
@@ -1,6 +1,5 @@
 package org.scalawiki
 
-
 trait LoginInfo {
   def login: String
 
@@ -11,13 +10,10 @@ case class LoginInfoValue(login: String, password: String) extends LoginInfo
 
 object LoginInfo {
 
-  def fromEnv(
-               loginProp: String = "SCALAWIKI_LOGIN",
-               passwordProp: String = "SCALAWIKI_PASSWORD"): Option[LoginInfo] =
-    for (
-      login <- sys.env.get(loginProp);
-      password <- sys.env.get(passwordProp)
-    ) yield
-      LoginInfoValue(login, password)
+  def fromEnv(loginProp: String = "SCALAWIKI_LOGIN",
+              passwordProp: String = "SCALAWIKI_PASSWORD"): Option[LoginInfo] =
+    for (login <- sys.env.get(loginProp);
+         password <- sys.env.get(passwordProp))
+    yield LoginInfoValue(login, password)
 
 }
