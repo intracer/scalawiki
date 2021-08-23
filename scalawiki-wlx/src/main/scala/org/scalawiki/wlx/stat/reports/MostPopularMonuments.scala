df2f4475f8bcda68420873553380de140cc224fc
ilya <intracer@gmail.com>
2021-08-23 12:17:29 +0300

    MostPopularMonuments by authors + by regions

--- scalawiki-wlx/src/main/scala/org/scalawiki/wlx/stat/reports/MostPopularMonuments.scala
+++ scalawiki-wlx/src/main/scala/org/scalawiki/wlx/stat/reports/MostPopularMonuments.scala
@@ -26,39 +26,72 @@ class MostPopularMonuments(val stat: ContestStat) extends Reporter {
 
   def mostPopularMonumentsTable(imageDbs: Seq[ImageDB], totalImageDb: Option[ImageDB], monumentDb: MonumentDB): Table = {
     val imageDbsByYear = imageDbs.groupBy(_.contest.year)
-
     val yearSeq = imageDbsByYear.keys.toSeq.sorted
-    val numYears = yearSeq.size
 
-    val columns = Seq("Id", "Name") ++
-      totalImageDb.map(_ => Seq(s"$numYears years photos", s"$numYears years authors")).getOrElse(Seq.empty) ++
-      yearSeq.flatMap(year => Seq(s"$year photos", s"$year authors"))
+    val authorsColumns = Seq("N", "Id", "Name", "Category") ++
+      totalImageDb.map(_ => Seq("authors", "photos")).getOrElse(Seq.empty)
 
-    val photosCountTotal = totalImageDb.map(_.imageCountById)
-    val authorsCountTotal = totalImageDb.map(_.authorsCountById)
+    val photosCountTotal = totalImageDb.map(_.imageCountById).get
+    val authorsCountTotal = totalImageDb.map(_.authorsCountById).get
 
-    val photoCounts = yearSeq.map(year => imageDbsByYear(year).head.imageCountById)
-    val authorCounts = yearSeq.map(year => imageDbsByYear(year).head.authorsCountById)
+    val topAuthors = topN(100, authorsCountTotal)
 
-    val counts = Seq(photosCountTotal, authorsCountTotal).flatten ++
-      (0 until numYears).flatMap(i => Seq(photoCounts(i), authorCounts(i)))
-
-    val topPhotos = (photosCountTotal.toSet ++ photoCounts).flatMap(topN(12, _).toSet)
-    val topAuthors = (authorsCountTotal.toSet ++ authorCounts).flatMap(topN(12, _).toSet)
-
-    val allTop = topPhotos ++ topAuthors
-    val allTopOrdered = allTop.toSeq.sorted
-
-
-    val rows = allTopOrdered.map { id =>
+    val rows = topAuthors.zipWithIndex.map { case (id, index) =>
       val monument = monumentDb.byId(id).get
       Seq(
-        id,
-        monument.name.replaceAll("\\[\\[", "[[:uk:") + monument.galleryLink
-      ) ++ counts.map(_.getOrElse(id, 0).toString)
+        (index + 1).toString,
+        s"{{nobr|$id}}",
+        monument.name.replaceAll("\\[\\[", "[[:uk:"), monument.galleryLink
+      ) ++ Seq(authorsCountTotal.getOrElse(id, 0).toString, photosCountTotal.getOrElse(id, 0).toString)
     }
 
-    new Table(columns, rows, name)
+    Table(authorsColumns, rows, name + s" in ${stat.imageDbsByYear.size} years (${yearSeq.head} — ${yearSeq.last})")
+  }
+
+  def mostPopularMonumentsInRegions(totalImageDb: ImageDB, monumentDb: MonumentDB): Map[String, Table] = {
+    val authorsColumns = Seq("N", "Id", "Name", "Category") ++
+      Seq("authors", "photos")
+
+    val country = monumentDb.contest.country
+    monumentDb.regionIds.map { regionId =>
+      val regionName = country.regionName(regionId)
+
+      val shortRegionName = regionName
+        .replaceAll("область", "")
+        .replaceAll("Автономна Республіка", "АР")
+
+      val imageDb = ImageDB(totalImageDb.contest, totalImageDb.imagesByRegion(regionId), Some(monumentDb))
+
+      val photosCountTotal = imageDb.imageCountById
+      val authorsCountTotal = imageDb.authorsCountById
+
+      val topAuthors = topN(5, authorsCountTotal)
+
+      val rows = topAuthors.zipWithIndex.map { case (id, index) =>
+        val monument = monumentDb.byId(id).get
+        Seq(
+          (index + 1).toString,
+          s"{{nobr|$id}}",
+          monument.name.replaceAll("\\[\\[", "[[:uk:"), monument.galleryLink
+        ) ++ Seq(authorsCountTotal.getOrElse(id, 0).toString, photosCountTotal.getOrElse(id, 0).toString)
+      }
+
+      shortRegionName -> Table(authorsColumns, rows)
+    }.toMap
+  }
+
+
+  override def asText: String = {
+    val header = s"\n==$name==\n"
+
+    val categoryText = s"\n[[Category:$category]]"
+
+    val byRegions = mostPopularMonumentsInRegions(stat.totalImageDb.get, stat.monumentDb.get).toSeq.sortBy(_._1).map{
+      case (name, table) =>
+        s"\n==$name==\n" + table.asWiki
+    }.mkString
+
+    header + table.asWiki + byRegions + categoryText
   }
 
   def topN(n: Int, stat: Map[String, Int]) = stat.toSeq.sortBy(-_._2).take(n).map(_._1)
