b53fbe9fd853640d12006f0cb78f7b0b0ff657e8
ilya <intracer@gmail.com>
2023-08-04 06:41:34 +0300

    some formatting

--- scalawiki-core/src/main/scala/org/scalawiki/query/PageQuery.scala
+++ scalawiki-core/src/main/scala/org/scalawiki/query/PageQuery.scala
@@ -7,17 +7,24 @@ import scala.concurrent.Future
 
 trait PageQuery {
 
-  def revisions(namespaces: Set[Int] = Set.empty, props: Set[String] = Set.empty, continueParam: Option[(String, String)] = None): Future[Iterable[Page]]
+  def revisions(
+      namespaces: Set[Int] = Set.empty,
+      props: Set[String] = Set.empty,
+      continueParam: Option[(String, String)] = None): Future[Iterable[Page]]
 
 }
 
 object PageQuery {
 
-  def byTitles(titles: Set[String], bot: MwBot): PageQuery = new PageQueryImplDsl(Right(titles), bot)
+  def byTitles(titles: Set[String], bot: MwBot): PageQuery =
+    new PageQueryImplDsl(Right(titles), bot)
 
-  def byTitle(title: String, bot: MwBot): SinglePageQuery = new PageQueryImplDsl(Right(Set(title)), bot)
+  def byTitle(title: String, bot: MwBot): SinglePageQuery =
+    new PageQueryImplDsl(Right(Set(title)), bot)
 
-  def byIds(ids: Set[Long], bot: MwBot):PageQuery = new PageQueryImplDsl(Left(ids), bot)
+  def byIds(ids: Set[Long], bot: MwBot): PageQuery =
+    new PageQueryImplDsl(Left(ids), bot)
 
-  def byId(id: Long, bot: MwBot): SinglePageQuery = new PageQueryImplDsl(Left(Set(id)), bot)
+  def byId(id: Long, bot: MwBot): SinglePageQuery =
+    new PageQueryImplDsl(Left(Set(id)), bot)
 }
