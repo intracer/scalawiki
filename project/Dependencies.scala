f0489092314a639c7b6f3b56db87e4f656a9d60e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-12-17 13:04:02 +0100

    Update logback-classic to 1.4.11 (#435)
    Co-authored-by: Ilya <intracer@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   val JimFsV = "1.3.0"
   val JodaTimeV = "2.12.2"
   val JSoupV = "1.16.2"
-  val LogbackClassicV = "1.3.14"
+  val LogbackClassicV = "1.4.11"
   val MockServerV = "5.15.0"
   val ReactiveStreamsV = "1.0.4"
   val RetryV = "0.3.6"
