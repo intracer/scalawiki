70ae84f4135cf89e466e07bd1cdb23ca29be98f7
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-03-09 09:50:30 +0100

    Update scala-library to 2.13.13 (#494)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val MockServerV = "5.15.0"
   val ReactiveStreamsV = "1.0.4"
   val RetryV = "0.3.6"
-  val Scala213V = "2.13.12"
+  val Scala213V = "2.13.13"
   val ScalaChartV = "0.8.0"
   val ScalaCheckV = "1.17.0"
   val ScalaCsvV = "1.3.10"
