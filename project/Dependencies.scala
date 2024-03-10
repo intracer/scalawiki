2132b6202b2a031736f4ed2f44e150a66de341eb
Scala Steward <scala_steward@virtuslab.com>
2024-03-10 14:48:58 +0000

    Update xwiki-commons-blame-api to 16.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val AaltoXmlV = "1.3.2"
   val BetterFilesV = "3.9.2"
-  val BlameApiV = "15.10.7"
+  val BlameApiV = "16.1.0"
   val ChronicleMapV = "3.22.9"
   val ChronoScalaV = "1.0.0"
   val FicusV = "1.5.2"
