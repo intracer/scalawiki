5726d73f19b2531e085472e3172ec5c879e09041
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-03-16 11:31:30 +0100

    Update commons-compress to 1.26.1 (#501)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -106,7 +106,7 @@ object Dependencies {
 
     object Commons {
       val CommonsCodecV = "1.16.1"
-      val CommonsCompressV = "1.26.0"
+      val CommonsCompressV = "1.26.1"
       val CommonsLang3V = "3.7"
       val CommonsIoV = "2.15.1"
 
