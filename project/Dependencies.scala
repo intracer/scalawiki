e5838f2c38684d18429bf2783f64b72e654db95a
Scala Steward <scala_steward@virtuslab.com>
2025-03-01 16:15:37 +0000

    Update jackson-annotations, jackson-core, ... to 2.18.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -118,7 +118,7 @@ object Dependencies {
     }
 
     object Jackson {
-      val JacksonV = "2.18.2"
+      val JacksonV = "2.18.3"
 
       val core = "com.fasterxml.jackson.core" % "jackson-core" % JacksonV
       val annotations =
